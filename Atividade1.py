06605583843bd7a4540a0d8374e0de798b54de34
EsterLemos33 <esterlemos445@gmail.com>
2025-08-08 15:35:26 -0300

    Create Atividade1.py
    Primeira versão do código da atividade 1 do curso SG Python.

---
+++ Atividade1.py
@@ -0,0 +1,18 @@
+print"eu sei fazer print assim"
+print("e tambem sei fazer assim")
+
+nome = 'Ester Alves Lemos'
+idade = "23 anos"
+print( 'Ester Alves Lemos ' + "23 anos")
+
+
+idade_n = 23
+peso = 62
+produto = idade_n * peso
+print( "Produto da idade pelo peso: ")
+print( produto)
+
+
+resto = peso % idade_n
+print( 'Resto da divisao do peso pela idade: ')
+print( resto)

12560ac09a04de1709f1bd0ee46f1682f1eda940
EsterLemos33 <esterlemos445@gmail.com>
2025-08-22 14:55:13 -0300

    Create Atividade2.py

---
+++ Atividade2.py
@@ -0,0 +1,13 @@
+idade= 24
+txtid= "Minha idade"
+print(txtid , idade)
+
+
+altura= "1.56"
+idade += float(altura)
+#print("e minha altura", altura, "somada a minha idade resulta em", idade)
+
+
+altura= True
+idade= False
+#print("Depois de executar o programa", txtid, idade, "e minha altura", altura)
